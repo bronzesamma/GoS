553447942286f88e32b6e0cb81618025cc5033ab
bronzesamma <romanov.muller@gmail.com>
2017-05-02 21:52:25 -0300

    Update PrimeAshe.lua

--- PrimeAshe.lua
+++ PrimeAshe.lua
@@ -8,34 +8,44 @@ function Ashe:__init()
 end
 
 function Ashe:LoadSpells()
-	W = { range = myHero:GetSpellData(_W).range, delay = myHero:GetSpellData(_W).delay, speed = myHero:GetSpellData(_W).speed, width = 80}
-	R = { range = 2000, delay = myHero:GetSpellData(_R).delay, speed = myHero:GetSpellData(_R).speed, width = myHero:GetSpellData(_R).width}
+	Q = { range = myHero:GetSpellData(_Q).range, delay = myHero:GetSpellData(_Q).delay, speed = myHero:GetSpellData(_Q).speed, width = myHero:GetSpellData(_Q).width }
+	W = { range = myHero:GetSpellData(_W).range, delay = myHero:GetSpellData(_W).delay, speed = myHero:GetSpellData(_W).speed, width = myHero:GetSpellData(_W).width }
+	E = { range = myHero:GetSpellData(_E).range, delay = myHero:GetSpellData(_E).delay, speed = myHero:GetSpellData(_E).speed, width = myHero:GetSpellData(_E).width }
+	R = { range = myHero:GetSpellData(_R).range, delay = myHero:GetSpellData(_R).delay, speed = myHero:GetSpellData(_R).speed, width = myHero:GetSpellData(_R).width }
 end
 
 function Ashe:LoadMenu()
-	local MenuIcons = "http://static.lolskill.net/img/champions/64/ashe.png"
-	local SpellIcons = { Q = "http://static.lolskill.net/img/abilities/64/Ashe_Q.png",
-						 W = "http://static.lolskill.net/img/abilities/64/Ashe_W.png",
-						 E = "http://static.lolskill.net/img/abilities/64/Ashe_E.png",
-						 R = "http://static.lolskill.net/img/abilities/64/Ashe_R.png", }
 	--Main Menu
-	self.Menu = MenuElement({type = MENU, id = "Menu", name = "Ashe", leftIcon = MenuIcons})
+	self.Menu = MenuElement({type = MENU, id = "Menu", name = "Ashe"})
 	
 	--Main Menu-- Ashe
 	self.Menu:MenuElement({type = MENU, id = "Mode", name = "Prime Ashe"})
 	--Main Menu-- Ashe -- Combo
 	self.Menu.Mode:MenuElement({type = MENU, id = "Combo", name = "Combo"})
-	self.Menu.Mode.Combo:MenuElement({id = "Q", name = "Use Q", value = true, leftIcon = SpellIcons.Q})
-	self.Menu.Mode.Combo:MenuElement({id = "W", name = "Use W", value = true, leftIcon = SpellIcons.W})
-	self.Menu.Mode.Combo:MenuElement({id = "R", name = "Use R KS combo", value = true, tooltip = "R > W > AA > AA", leftIcon = SpellIcons.R})
+	self.Menu.Mode.Combo:MenuElement({id = "Q", name = "Use Q", value = true})
+	self.Menu.Mode.Combo:MenuElement({id = "W", name = "Use W", value = true})
+	self.Menu.Mode.Combo:MenuElement({id = "R", name = "Use R KS combo", value = true, tooltip = "R > W > AA > AA"})
 	--Main Menu-- Ashe -- KS
 	self.Menu.Mode:MenuElement({type = MENU, id = "KS", name = "Killsteal"})
-	self.Menu.Mode.KS:MenuElement({id = "R", name = "Use R", value = true, leftIcon = SpellIcons.R})
+	self.Menu.Mode.KS:MenuElement({id = "R", name = "Use R", value = true})
+	--Main Menu-- Ashe -- Harass
+	self.Menu.Mode:MenuElement({type = MENU, id = "Harass", name = "Harass"})
+	self.Menu.Mode.Harass:MenuElement({id = "Q", name = "Use Q", value = true})
+	self.Menu.Mode.Harass:MenuElement({id = "W", name = "Use W", value = true})
+	self.Menu.Mode.Harass:MenuElement({type = MENU, id = "MM", name = "Mana Manager"})
+	self.Menu.Mode.Harass.MM:MenuElement({id = "Mana", name = "Min Mana to Harass(%)", value = 40, min = 0, max = 100, step = 1})
 	--Main Menu-- Ashe -- LaneClear
 	self.Menu.Mode:MenuElement({type = MENU, id = "LaneClear", name = "Lane Clear"})
-	self.Menu.Mode.LaneClear:MenuElement({id = "Q", name = "Use Q", value = true, leftIcon = SpellIcons.Q})
+	self.Menu.Mode.LaneClear:MenuElement({id = "Q", name = "Use Q", value = true})
+	self.Menu.Mode.LaneClear:MenuElement({id = "W", name = "Use W", value = true})
 	self.Menu.Mode.LaneClear:MenuElement({type = MENU, id = "MM", name = "Mana Manager"})
-	self.Menu.Mode.LaneClear.MM:MenuElement({id = "QMana", name = "Min Mana to Q in Lane Clear(%)", value = 40, min = 0, max = 100, step = 1})
+	self.Menu.Mode.LaneClear.MM:MenuElement({id = "Mana", name = "Min Mana to Lane Clear(%)", value = 40, min = 0, max = 100, step = 1})
+	--Main Menu-- Ashe -- JungleClear
+	self.Menu.Mode:MenuElement({type = MENU, id = "JungleClear", name = "Jungle Clear"})
+	self.Menu.Mode.JungleClear:MenuElement({id = "Q", name = "Use Q", value = true})
+	self.Menu.Mode.JungleClear:MenuElement({id = "W", name = "Use W", value = true})
+	self.Menu.Mode.JungleClear:MenuElement({type = MENU, id = "MM", name = "Mana Manager"})
+	self.Menu.Mode.JungleClear.MM:MenuElement({id = "Mana", name = "Min Mana to Jungle Clear(%)", value = 40, min = 0, max = 100, step = 1})
 	--Main Menu-- Ashe -- Spell Range 
 	self.Menu:MenuElement({type = MENU, id = "Drawing", name = "Spell Range"})
 	self.Menu.Drawing:MenuElement({id = "W", name = "Draw W Range", value = true})
@@ -46,10 +56,13 @@ end
 function Ashe:Tick()
 	local Combo = (_G.SDK and _G.SDK.Orbwalker.Modes[_G.SDK.ORBWALKER_MODE_COMBO]) or (_G.GOS and _G.GOS:GetMode() == "Combo") or (_G.EOWLoaded and EOW:Mode() == "Combo")
 	local Clear = (_G.SDK and _G.SDK.Orbwalker.Modes[_G.SDK.ORBWALKER_MODE_LANECLEAR]) or (_G.SDK and _G.SDK.Orbwalker.Modes[_G.SDK.ORBWALKER_MODE_JUNGLECLEAR]) or (_G.GOS and _G.GOS:GetMode() == "Clear") or (_G.EOWLoaded and EOW:Mode() == "LaneClear")
+	local Harass = (_G.SDK and _G.SDK.Orbwalker.Modes[_G.SDK.ORBWALKER_MODE_HARASS]) or (_G.GOS and _G.GOS:GetMode() == "Harass") or (_G.EOWLoaded and EOW:Mode() == "Harass")
 	if Combo then
 		self:Combo()
 	elseif Clear then
-		self:Clear()		
+		self:Clear()
+	elseif Harass then
+		self:Harass()		
 	end	
 		self:KS()
 end
@@ -116,7 +129,7 @@ function Ashe:Combo()
 	local target =  (_G.SDK and _G.SDK.TargetSelector:GetTarget(1500, _G.SDK.DAMAGE_TYPE_PHYSICAL)) or (_G.GOS and _G.GOS:GetTarget(1500,"AD")) or ( _G.EOWLoaded and EOW:GetTarget())	
 	    
 	    if self:IsValidTarget(target,1200) and self.Menu.Mode.Combo.W:Value() and target:GetCollision(W.width,W.speed,W.delay) == 0 and self:isReady(_W) and myHero.attackData.state == STATE_WINDDOWN then
-			Control.CastSpell(HK_W,target)
+			Control.CastSpell(HK_W,target:GetPrediction(W.speed, W.delay))
 	    end
 
 		if self:IsValidTarget(target,550) and self.Menu.Mode.Combo.Q:Value() and self:isReady(_Q) and myHero.attackData.state == STATE_WINDDOWN then
@@ -128,7 +141,7 @@ function Ashe:Combo()
 			local level = myHero:GetSpellData(_R).level
 			local Rdmg = ({200, 400, 600})[level] + myHero.ap + ({20, 35, 50, 65, 80})[levelW] + myHero.totalDamage * 2 
 			if Rdmg >= self:HpPred(target,1) + target.hpRegen * 2 then
-				Control.CastSpell(HK_R,target)
+				Control.CastSpell(HK_R,target:GetPrediction(R.speed, R.delay))
 			end
 	end
 end
@@ -150,19 +163,46 @@ function Ashe:KS()
 	end
 end
 
+function Ashe:Harass()
+
+	if self:GetValidEnemy(1200) == false then return end
+	
+	if (not _G.SDK and not _G.GOS and not _G.EOWLoaded) then return end
+	
+	local target =  (_G.SDK and _G.SDK.TargetSelector:GetTarget(1200, _G.SDK.DAMAGE_TYPE_PHYSICAL)) or (_G.GOS and _G.GOS:GetTarget(1200,"AD")) or ( _G.EOWLoaded and EOW:GetTarget())
+		
+	    if self:IsValidTarget(target,600) and (myHero.mana/myHero.maxMana >= self.Menu.Mode.Harass.MM.Mana:Value() / 100) and self.Menu.Mode.Harass.Q:Value() and self:isReady(_Q) and not myHero.isChanneling  then
+			Control.CastSpell(HK_Q)
+		end
+		if self:IsValidTarget(target,1200) and (myHero.mana/myHero.maxMana >= self.Menu.Mode.Harass.MM.Mana:Value() / 100) and self.Menu.Mode.Harass.W:Value() and self:isReady(_W) and not myHero.isChanneling  then
+			Control.CastSpell(HK_W,target:GetPrediction(W.speed, W.delay))
+		end
+end
+
 function Ashe:Clear()
 
-	if self:GetValidMinion(900) == false then return end
+	if self:GetValidMinion(1200) == false then return end
 		for i = 1, Game.MinionCount() do
 		local minion = Game.Minion(i)
 		if  minion.team == 200 then
-			if self:IsValidTarget(minion,600) and (myHero.mana/myHero.maxMana >= self.Menu.Mode.LaneClear.MM.QMana:Value() / 100) and self.Menu.Mode.LaneClear.Q:Value() and self:isReady(_Q) then
-				if self:CountEnemyMinions(600) >= 4 then
+			if self:IsValidTarget(minion,600) and (myHero.mana/myHero.maxMana >= self.Menu.Mode.LaneClear.MM.Mana:Value() / 100) and self.Menu.Mode.LaneClear.Q:Value() and self:isReady(_Q) then
+					Control.CastSpell(HK_Q)
+				break
+			end
+			if self:IsValidTarget(minion,1200) and (myHero.mana/myHero.maxMana >= self.Menu.Mode.LaneClear.MM.Mana:Value() / 100) and self.Menu.Mode.LaneClear.W:Value() and self:isReady(_W) then
+					Control.CastSpell(HK_W,target)
+				break
+			end
+		elseif minion.team == 300 then
+			if self:IsValidTarget(minion,600) and (myHero.mana/myHero.maxMana >= self.Menu.Mode.JungleClear.MM.Mana:Value() / 100) and self.Menu.Mode.JungleClear.Q:Value() and self:isReady(_Q) then
 					Control.CastSpell(HK_Q)
 				break
 				end
 			end
-		end	
+			if self:IsValidTarget(minion,1200) and (myHero.mana/myHero.maxMana >= self.Menu.Mode.JungleClear.MM.Mana:Value() / 100) and self.Menu.Mode.JungleClear.W:Value() and self:isReady(_W) then
+					Control.CastSpell(HK_W,target)
+				break
+			end
 	end
 end
 
